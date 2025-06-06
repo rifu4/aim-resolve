509fc62b6417ab0d92a14a3315395fc07207b04f
Richard Fuchs <richard.fuchs@tum.de>
2025-06-06 12:41:16 +0200

    fix train script

--- steering/train/train_uno.py
+++ steering/train/train_uno.py
@@ -1,26 +1,12 @@
 import os
-# os.environ['JAX_PLATFORM_NAME'] = 'gpu'
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-# os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.9'
-# os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
-
-
-import torch
-import matplotlib.pyplot as plt
 import sys
-import torch.nn.functional as F
-from neuralop.models import UNO
+import torch
 from neuralop import Trainer
-from torch.optim import AdamW
+from neuralop.models import UNO
 from neuralop.utils import count_model_params
+from torch.optim import AdamW
 
-import numpy as np
-
-from aim_resolve import Dataset, BCELoss, yaml_load, plot_arrays
-
-# torch.cuda.set_device(1)
-
-# device = "cuda:1" if torch.cuda.is_available() else "cpu"
+from aim_resolve import Dataset, BCELoss, yaml_load, yaml_save, plot_arrays
 
 
 
@@ -28,48 +14,48 @@ _, yfile = sys.argv[0], sys.argv[1]
 
 dct = yaml_load(yfile)
 
+device = dct['device']
+name = dct['name']
+odir = dct['odir']
+
+yaml_save(dct, os.path.join(odir, name + '.yml'))
+
+
+# load the dataset and setup the dataloaders
 dataset = Dataset.build(**dct['dataset'])
 
 train_loader = dataset.train_loader(**dct['dataloader']['train'])
 valid_loaders = dataset.valid_loader(**dct['dataloader']['valid'])
 
 
+# setup the UNO model
 model = UNO(**dct['model'])
-model = model.to(dct['device'])
-
+model = model.to(device)
 n_params = count_model_params(model)
-print(f'\nOur model has {n_params} parameters.')
-sys.stdout.flush()
+print('\nModel: \n', model)
+print('n params:', n_params)
 
 
-#Create the optimizer
-optimizer = AdamW(
-    model.parameters(), 
-    lr=8e-3, 
-    weight_decay=1e-4,
-)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
+# setup optimizer and scheduler
+optimizer = AdamW(model.parameters(), **dct['optimizer'])
+print('\nOptimizer: \n', optimizer)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, **dct['scheduler'])
 
 
+# setup the loss functions
 train_loss = BCELoss()
 valid_losses = {'bce': BCELoss()}
+print(f'\nLosses: \n   train: {train_loss}\n   valid: {valid_losses}')
 
 
-print('\n### MODEL ###\n', model)
-print('\n### OPTIMIZER ###\n', optimizer)
-print('\n### SCHEDULER ###\n', scheduler)
-print('\n### LOSSES ###')
-print(f'\n * Train: {train_loss}')
-print(f'\n * Valid: {valid_losses}')
-
-
-# Create the trainer
+# setup the trainer
 trainer = Trainer(
     model=model,
-    device=dct['device'],
+    device=device,
     **dct['trainer'],
 )
 
+# train the model
 trainer.train(
     train_loader=train_loader,
     test_loaders=valid_loaders,
@@ -81,28 +67,26 @@ trainer.train(
 )
 
 
-# However, for practicity, we created a minimal example that
-# i) fits in just a few Mb of memory
-# ii) can be trained quickly on CPU
-#
-# In practice we would train a Neural Operator on one or multiple GPUs
+# save the model
+torch.save(model.state_dict(), os.path.join(odir, name + '.pth'))
 
-# test_samples = test_loaders['128'].dataset
-test_samples = train_loader.dataset
 
-n = 5
+# plot the predictions for training and validation datasets
 
-val = []
-for index in range(n):
-    data = test_samples[index]
-    x = data['x']
-    x = torch.from_numpy(x)
-    out = model(x.unsqueeze(0).to(dct['device'])).cpu()
+n_copies = dct['plot'].pop('n_copies', 5)
+
+arrays = []
+for i in range(n_copies):
+    data = train_loader.dataset[i]
+    x = torch.from_numpy(data['x'])
+    pred = model(x.unsqueeze(0).to(device)).cpu()
     y = data['y']
-    val += [x[0].detach().numpy(), y[0], out.detach().numpy()[0,0].clip(0,1), y[1], out.detach().numpy()[0,1].clip(0,1)]
+    arrays += [x[0].detach().numpy(), y[0], pred.detach().numpy()[0,0].clip(0,1), y[1], pred.detach().numpy()[0,1].clip(0,1)]
 
 plot_arrays(
-    val,
-    label = n * ['Input', 'True Points', 'Predicted Points', 'True Objects', 'Predicted Objects'],
-    cols = 5,
+    arrays,
+    label = n_copies * ['Input', 'True Points', 'Predicted Points', 'True Objects', 'Predicted Objects'],
+    name = name + '_train',
+    cols = n_copies,
+    **dct['plot']
 )
