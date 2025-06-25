5312da33344fb7dfb6079b8ff4f7f8d4d436fd9b
Richard Fuchs <richard.fuchs@tum.de>
2025-06-25 15:16:12 +0200

    use click for gpu support

--- steering/data/data_gen.py
+++ steering/data/data_gen.py
@@ -1,43 +1,48 @@
 import os
-os.environ['JAX_PLATFORM_NAME'] = 'cpu'
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-# os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.9'
-# os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+import click
 
-import jax
-import sys
-from jax import random
-from aim_resolve import ImageDataGenerator, yaml_load
+@click.command()
+@click.option('--config', required=True, help='Path to the YAML config file')
+@click.option('--cuda_device', default='', help='CUDA device to use (e.g. "0", "0,1", ...), default is "" for CPU')
 
-print(jax.devices())
-jax.config.update("jax_enable_x64", True)
+def main(config, cuda_device):
+    if str(cuda_device) == '':
+        os.environ['JAX_PLATFORM_NAME'] = 'cpu'
+    else:
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(cuda_device)
+        os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.9'
+        os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+
+    import jax
+    from jax import random
+    from aim_resolve import ImageDataGenerator, yaml_load
+
+    jax.config.update("jax_enable_x64", True)
+
+    # Load config
+    dct = yaml_load(config)
+
+    # Generate or load data
+    if 'parameters' in dct:
+        print('\ngenerate data:')
+        data = ImageDataGenerator.build(parameters=dct['parameters'])
+
+        key = random.PRNGKey(dct['seed'])
+        key, subkey = random.split(key)
+
+        data.draw_samples(subkey, dct['n_copies'], dct['batch_size'])
+        data.save(dct['name'], dct['odir'], dct['dtype'])
+
+    else:
+        print('\nload data:\nfname:', dct['name'])
+        data = ImageDataGenerator.load(dct['name'], dct['odir'])
+
+    # Optionally plot
+    if 'plot' in dct:
+        print('\nplot data:')
+        data.plot_samples(dct['name'], **dct['plot'])
+        print('done')
 
 
-
-_, yfile = sys.argv[0], sys.argv[1]
-
-dct = yaml_load(yfile)
-
-
-# generate and save data or load existing data
-if 'parameters' in dct:
-    print('\ngenerate data:')
-    data = ImageDataGenerator.build(parameters=dct['parameters'])
-
-    key = random.PRNGKey(dct['seed'])
-    key, subkey = random.split(key)
-
-    data.draw_samples(subkey, dct['n_copies'])
-
-    data.save(dct['name'], dct['odir'], dct['dtype'])
-
-else:
-    print('\nload data:\nfname:', dct['name'])
-    data = ImageDataGenerator.load(dct['name'], dct['odir'])
-    
-
-# plot data
-if 'plot' in dct:
-    print('\nplot data:')
-    data.plot_samples(dct['name'], **dct['plot'])
-    print('done')
+if __name__ == "__main__":
+    main()
