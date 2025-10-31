c472dda7dba7da38d53c614351c31b2aa9c0994f
Richard Fuchs <richard.fuchs@tum.de>
2025-10-31 14:58:12 +0100

    compute on cpu for np.arrays

--- src/aim_resolve/model/map.py
+++ src/aim_resolve/model/map.py
@@ -1,4 +1,6 @@
+import jax
 import jax.numpy as jnp
+import numpy as np
 from jax import vmap
 from jax.lax import dynamic_slice, dynamic_update_slice, fori_loop
 from functools import partial
@@ -38,7 +40,7 @@ def map_signal(in_grid, out_grid):
             map_function = map_point_2d
         case _:
             raise TypeError('Mapping only supported from SignalGrid to SignalGrid, or from PointGrid to SignalGrid.')
-        
+
     fun = partial(map_array,
         map_function=map_function,
         in_copies=in_grid.n_copies,
@@ -54,64 +56,78 @@ def map_signal(in_grid, out_grid):
 
 
 def map_array(in_array, map_function, in_copies, out_copies, in_shape, out_shape, in_start, out_start, zoom):
-    in_array = jnp.asarray(in_array)
-    in_start = jnp.asarray(in_start)
-    out_start = jnp.asarray(out_start)
-    match (in_array.ndim, in_copies, out_copies):
-        # (x, y) -> (x', y')
-        case (2, 1, 1):
-            out_array = jnp.zeros(out_shape, dtype=in_array.dtype)
-            return map_function(in_array, out_array, in_start, out_start, in_shape, zoom)
-        # (x, y) -> (n_out, x', y')
-        case (2, 1, n_out):
-            out_array = jnp.zeros((n_out,) + out_shape, dtype=in_array.dtype)
-            loop_over_n_out = lambda i,o: o.at[i].set(map_function(in_array, o[i], in_start[i], out_start[i], in_shape, zoom))
-            return fori_loop(0, n_out, loop_over_n_out, out_array)
+    if isinstance(in_array, np.ndarray):
+        is_numpy = True
+        device = jax.devices('cpu')[0]
+    else:
+        is_numpy = False
+        device = jax.devices()[0]
 
-        # (n/f, x, y) -> (n/f, x', y')
-        case (3, 1, 1):
-            n = in_array.shape[0]
-            out_array = jnp.zeros((n,) + out_shape, dtype=in_array.dtype)
-            return vmap(map_function, in_axes=(0, 0, None, None, None, None))(in_array, out_array, in_start, out_start, in_shape, zoom)
-        # (n_in, x, y) -> (x', y')
-        case (3, n_in, 1):
-            out_array = jnp.zeros(out_shape, dtype=in_array.dtype)
-            loop_over_n_in = lambda i,o : map_function(in_array[i], o, in_start[i], out_start[i], in_shape, zoom)
-            return fori_loop(0, n_in, loop_over_n_in, out_array)
-        # (n_in, x, y) -> (n_out, x', y') if n_in == n_out
-        case (3, n_in, n_out) if n_in == n_out:
-            out_array = jnp.zeros((n_out,) + out_shape, dtype=in_array.dtype)
-            loop_over_n_out = lambda i,o: o.at[i].set(map_function(in_array[i], o[i], in_start[i], out_start[i], in_shape, zoom))
-            return fori_loop(0, n_out, loop_over_n_out, out_array)
-        # (f, x, y) -> (n_out, f, x, y)
-        case (3, 1, n_out):
-            f = in_array.shape[0]
-            out_array = jnp.zeros((n_out, f) + out_shape, dtype=in_array.dtype)
-            vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
-            loop_over_n_out = lambda i,o: o.at[i].set(vmap_over_f(in_array, o[i], in_start[i], out_start[i], in_shape, zoom))
-            return fori_loop(0, n_out, loop_over_n_out, out_array)
+    with jax.default_device(device):
+        in_array = jnp.asarray(in_array)
+        in_start = jnp.asarray(in_start)
+        out_start = jnp.asarray(out_start)
 
-        # (n, f, x, y) -> (n, f, x', y')
-        case (4, 1, 1):
-            n, f = in_array.shape[:2]
-            out_array = jnp.zeros((n, f) + out_shape, dtype=in_array.dtype)
-            vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
-            loop_over_n = lambda i,o: o.at[i].set(vmap_over_f(in_array[i], o[i], in_start, out_start, in_shape, zoom))
-            return fori_loop(0, n, loop_over_n, out_array)
-        # (n_in, f, x, y) -> (f, x', y')
-        case (4, n_in, 1):
-            f = in_array.shape[1]
-            out_array = jnp.zeros((f,) + out_shape, dtype=in_array.dtype)
-            vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
-            loop_over_n_in = lambda i,o : vmap_over_f(in_array[i], o, in_start[i], out_start[i], in_shape, zoom)
-            return fori_loop(0, n_in, loop_over_n_in, out_array)
-        # (n_in, f, x, y) -> (n_out, f, x', y') if n_in == n_out
-        case (4, n_in, n_out) if n_in == n_out:
-            f = in_array.shape[1]
-            out_array = jnp.zeros((n_out, f) + out_shape, dtype=in_array.dtype)
-            vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
-            loop_over_n_out = lambda i,o: o.at[i].set(vmap_over_f(in_array[i], o[i], in_start[i], out_start[i], in_shape, zoom))
-            return fori_loop(0, n_out, loop_over_n_out, out_array)    
+        match (in_array.ndim, in_copies, out_copies):
+            # (x, y) -> (x', y')
+            case (2, 1, 1):
+                out_array = jnp.zeros(out_shape, dtype=in_array.dtype)
+                res = map_function(in_array, out_array, in_start, out_start, in_shape, zoom)
+            # (x, y) -> (n_out, x', y')
+            case (2, 1, n_out):
+                out_array = jnp.zeros((n_out,) + out_shape, dtype=in_array.dtype)
+                loop_over_n_out = lambda i,o: o.at[i].set(map_function(in_array, o[i], in_start[i], out_start[i], in_shape, zoom))
+                res = fori_loop(0, n_out, loop_over_n_out, out_array)
+
+            # (n/f, x, y) -> (n/f, x', y')
+            case (3, 1, 1):
+                n = in_array.shape[0]
+                out_array = jnp.zeros((n,) + out_shape, dtype=in_array.dtype)
+                res = vmap(map_function, in_axes=(0, 0, None, None, None, None))(in_array, out_array, in_start, out_start, in_shape, zoom)
+            # (n_in, x, y) -> (x', y')
+            case (3, n_in, 1):
+                out_array = jnp.zeros(out_shape, dtype=in_array.dtype)
+                loop_over_n_in = lambda i,o : map_function(in_array[i], o, in_start[i], out_start[i], in_shape, zoom)
+                res = fori_loop(0, n_in, loop_over_n_in, out_array)
+            # (n_in, x, y) -> (n_out, x', y') if n_in == n_out
+            case (3, n_in, n_out) if n_in == n_out:
+                out_array = jnp.zeros((n_out,) + out_shape, dtype=in_array.dtype)
+                loop_over_n_out = lambda i,o: o.at[i].set(map_function(in_array[i], o[i], in_start[i], out_start[i], in_shape, zoom))
+                res = fori_loop(0, n_out, loop_over_n_out, out_array)
+            # (f, x, y) -> (n_out, f, x, y)
+            case (3, 1, n_out):
+                f = in_array.shape[0]
+                out_array = jnp.zeros((n_out, f) + out_shape, dtype=in_array.dtype)
+                vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
+                loop_over_n_out = lambda i,o: o.at[i].set(vmap_over_f(in_array, o[i], in_start[i], out_start[i], in_shape, zoom))
+                res = fori_loop(0, n_out, loop_over_n_out, out_array)
+
+            # (n, f, x, y) -> (n, f, x', y')
+            case (4, 1, 1):
+                n, f = in_array.shape[:2]
+                out_array = jnp.zeros((n, f) + out_shape, dtype=in_array.dtype)
+                vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
+                loop_over_n = lambda i,o: o.at[i].set(vmap_over_f(in_array[i], o[i], in_start, out_start, in_shape, zoom))
+                res = fori_loop(0, n, loop_over_n, out_array)
+            # (n_in, f, x, y) -> (f, x', y')
+            case (4, n_in, 1):
+                f = in_array.shape[1]
+                out_array = jnp.zeros((f,) + out_shape, dtype=in_array.dtype)
+                vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
+                loop_over_n_in = lambda i,o : vmap_over_f(in_array[i], o, in_start[i], out_start[i], in_shape, zoom)
+                res = fori_loop(0, n_in, loop_over_n_in, out_array)
+            # (n_in, f, x, y) -> (n_out, f, x', y') if n_in == n_out
+            case (4, n_in, n_out) if n_in == n_out:
+                f = in_array.shape[1]
+                out_array = jnp.zeros((n_out, f) + out_shape, dtype=in_array.dtype)
+                vmap_over_f = vmap(map_function, in_axes=(0, 0, None, None, None, None))
+                loop_over_n_out = lambda i,o: o.at[i].set(vmap_over_f(in_array[i], o[i], in_start[i], out_start[i], in_shape, zoom))
+                res = fori_loop(0, n_out, loop_over_n_out, out_array)  
+
+        if is_numpy:
+            res = np.asarray(res)
+        
+    return res
 
 
 
