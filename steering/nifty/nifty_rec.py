0a701fc4fcc4f23dc93971810faf5cf514e8ecde
Richard Fuchs <richard.fuchs@tum.de>
2025-11-12 15:38:22 +0100

    new plotting including spectral index

--- steering/nifty/nifty_rec.py
+++ steering/nifty/nifty_rec.py
@@ -2,11 +2,12 @@ import os
 import click
 
 @click.command()
+@click.option('--base', default='config/base.yml', help='Path to the base YAML config file')
 @click.option('--config', required=True, help='Path to the YAML config file')
 @click.option('--mode', required=True, help='Mode for NIFTy optimization, "exp", "radio" or "fast-radio"')
 @click.option('--cuda_device', default='', help='CUDA device to use (e.g. "0", "0,1", ...), default is "" for CPU')
 
-def main(config, mode, cuda_device):
+def main(base, config, mode, cuda_device):
     if str(cuda_device) == '':
         os.environ['JAX_PLATFORM_NAME'] = 'cpu'
     else:
@@ -21,11 +22,14 @@ def main(config, mode, cuda_device):
     jax.config.update("jax_enable_x64", True)
 
     # Instantiate the optimize-config class
-    cfg = aim.OptimizeKLConfig.from_file(('config/base.yml', config), aim.get_builders, mode)
+    cfg = aim.OptimizeKLConfig.from_file((base, config), aim.get_builders, mode)
     odir = cfg.sections['opt']['odir'] + '/plots'
 
     # Initialize all signal models for each iteration
-    sig_dct = {sec: cfg.instantiate_sec(sec) for sec in cfg.sections if sec.startswith('sky')}
+    sky_dct = {sec: cfg.instantiate_sec(sec) for sec in cfg.sections if sec.startswith('sky.')}
+    sky_models = []
+    for sky in sky_dct.values():
+        sky_models += [sky, ] + [m for m in sky.models if len(sky.models) > 1]
 
     # Print and plot the data
     data_dct = {sec: cfg.instantiate_sec(sec) for sec in cfg.sections if sec.startswith('data')}
@@ -34,21 +38,21 @@ def main(config, mode, cuda_device):
         if isinstance(dv, aim.ImageData):
             aim.plot_arrays(dv.val, name=dk, odir=odir, norm='log')
         elif isinstance(dv, aim.Observation):
-            aim.plot_arrays(dv.dirty_image(next(iter(sig_dct.values())).grid), name=dk, odir=odir)
+            aim.plot_arrays(dv.dirty_image(sky_models[0].grid), name=dk, odir=odir)
 
     # Define a callback function to plot the results of the optimization after each iteration
     def callback(samples, opt_state, *_):
-        for key,sig in sig_dct.items():
-            if aim.domain_keys(sig).issubset(aim.domain_keys(samples)):
-                sig_val = samples.mean(sig)
-                sig_min = sig_val.max()/1e4
-                aim.plot_arrays(sig_val, name=f'{opt_state.nit}_{key}', odir=odir, norm='log', rows=1, vmin=sig_min, vmax=sig_val.max())
-                if sig.freq.size > 1:
-                     sig_val = samples.mean(sig)
-                     alpha = samples.mean(sig.spectral_index)
-                     freq_idx = sig.freq.size // 2
-                     contours = {'array': sig_val[freq_idx], 'colors': 'white', 'levels': [sig_val.max() / d for d in [1e3, 1e2, 10]], 'linewidths': 0.5}
-                     aim.plot_arrays(np.where(sig_val[freq_idx] > sig_min, alpha, np.nan), name=f'{opt_state.nit}_{key}_alpha', odir=odir, contours=contours)
+        for sky in sky_models:
+            if aim.domain_keys(sky).issubset(aim.domain_keys(samples)):
+                sky_val = samples.mean(sky)
+                sky_min = sky_val.max()/1e4
+                aim.plot_arrays(sky_val, name=f'{opt_state.nit}_{sky.prefix}', odir=odir, norm='log', rows=1, vmin=sky_min)
+                if sky.freq.size > 1:
+                     sky_ref = sky_val[sky.freq.size // 2]
+                     aim.plot_arrays(sky_ref, name=f'{opt_state.nit}_{sky.prefix}_ref', odir=odir, norm='log', rows=1, vmin=sky_min)
+                     alpha = samples.mean(sky.spectral_index)
+                     contours = {'array': sky_ref, 'colors': 'white', 'levels': [sky_val.max() / d for d in [1e3, 1e2, 10]], 'linewidths': 0.25}
+                     aim.plot_arrays(np.where(sky_ref > sky_min, alpha, np.nan), name=f'{opt_state.nit}_{sky.prefix}_alpha', odir=odir, contour=contours)
 
     # Run the optimization
     samples, *_ = cfg.optimize_kl(callback=callback)
