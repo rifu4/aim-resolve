7265e01b8a8f8312f57e18621d4237ac9809205c
Richard Fuchs <richard.fuchs@tum.de>
2026-02-16 09:58:57 +0100

    exchange paper jupyter nbs by ipython scripts

---
+++ steering/paper/eso_mf_talk.py
@@ -0,0 +1,292 @@
+#%%
+import os
+os.environ['JAX_PLATFORM_NAME'] = 'cpu'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+# os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+
+#%%
+import jax
+import pickle
+import numpy as np
+from aim_resolve import OptimizeKLConfig, SetupKLConfig, SignalGrid, get_builders, plot_arrays, map_signal
+
+jax.config.update("jax_enable_x64", True)
+
+#%%
+def latest_model_file(directory):
+    import os
+    import re
+
+    max_file = None
+    max_it = -1
+    for filename in os.listdir(directory + '/files'):
+        match = re.compile(r'(\d+)_pre\.yml$').match(filename)
+        if match:
+            it = int(match.group(1))
+            if it > max_it:
+                max_it = it
+                max_file = filename
+
+    return directory + f'/files/{max_file}'
+
+
+
+def box_markers(cfg, ps_map, grid, it):
+    import numpy as np
+    from aim_resolve import draw_boxes
+
+    px, py = np.argwhere(ps_map == 1).T
+    ps_mrk = dict(x=px, y=py, s=10, c='white', marker='+')
+    box_map = draw_boxes(cfg.sections, grid, it)
+    ox, oy = np.argwhere(box_map == 1).T
+    oj_mrk = dict(x=ox, y=oy, s=0.1, c='white', marker=',')
+    
+    return dict(ps_mrk=ps_mrk, oj_mrk=oj_mrk)
+
+#%%
+dir = '/Users/rf/Development/packages/aim-resolve/steering/runs/fast_vi_1f_1024_2z_b'
+
+ARCMIN2RAD = np.pi / 60 / 180
+AS2RAD = ARCMIN2RAD / 60
+CONV_FACTOR = 1000 * AS2RAD**2
+
+
+eso_yml = latest_model_file(dir)
+print('load:', eso_yml)
+
+optim_cfg = OptimizeKLConfig.from_file((dir + '/files/base.yml', eso_yml), get_builders, 'total')
+optim_cfg.sections['data.0']['fname'] = '/Users/rf/Development/data/eso_986-1137mhz.npz'
+
+
+# obs = optim_cfg.instantiate_sec('data.0')
+# space = SignalGrid.build(**optim_cfg.sections['grid_bg'])
+# dirty = obs.dirty_image(space)
+# print('dirty image shape:', dirty.shape)
+
+sky_models = [sec for sec in optim_cfg.sections if sec.startswith('sky.')]
+print('sky components:', sky_models)
+
+sky_list, samples_list, sky_markers = [], [], []
+for it,sec in enumerate(sky_models):
+    sky_it = optim_cfg.instantiate_sec(sec)
+    sky_list.append(sky_it)
+    with open(dir + f'/opt/{it}_rec/last.pkl', "rb") as f:
+        smp_it, *_ = pickle.load(f)
+    samples_list.append(smp_it)
+
+    if it == 0:
+        sky_markers += [{}]
+    else:
+        setup_cfg = SetupKLConfig.from_file(eso_yml)
+        sky_ps = sky_it.points[0]
+        ps_map = map_signal(sky_ps.points.grid, sky_ps.grid)(np.ones(sky_ps.shape))
+        sky_markers += [box_markers(setup_cfg, ps_map, sky_it.grid, it), ]
+
+print('len sky_list:', len(sky_list))
+print('len samples_list:', len(samples_list))
+print('len sky_markers:', len(sky_markers))
+
+#%%
+mf_rec = '4_rec_4f'
+it = 3
+
+ARCMIN2RAD = np.pi / 60 / 180
+AS2RAD = ARCMIN2RAD / 60
+CONV_FACTOR = 1000 * AS2RAD**2
+
+
+opt_yml = f'{dir}/opt/{mf_rec}/opt.yml'
+print('load:', opt_yml)
+
+optim_cfg = OptimizeKLConfig.from_file(opt_yml, get_builders, 'major')
+optim_cfg.sections['data.0']['fname'] = '/Users/rf/Development/data/eso_986-1137mhz.npz'
+optim_cfg.sections[f'data.{it}']['fname'] = '/Users/rf/Development/data/eso_1356-1439mhz.npz'
+
+sky_mf = optim_cfg.instantiate_sec(f'sky.{it}')
+print(sky_mf.domain.tree.keys())
+
+with open(f'{dir}/opt/{mf_rec}/last.pkl', "rb") as f:
+    samples_mf, *_ = pickle.load(f)
+
+#%%
+plot_dict = dict(
+    name = None,
+    odir = None,
+    cmap = 'inferno',
+    cbar = False,
+    ticks = 0,
+)
+
+#%%
+sky_means, sky_stds = [], []
+for sky, smp in zip(sky_list, samples_list):
+    mean, std = smp.mean_and_std(sky)
+    sky_means += [mean * CONV_FACTOR, ]
+    sky_stds += [std * CONV_FACTOR, ]
+
+print('len sky_means:', len(sky_means))
+print('len sky_stds:', len(sky_stds))
+
+#%%
+mdl_it = 3
+
+array = sky_means[mdl_it-1]
+plot_markers = sky_markers[mdl_it-1]
+
+vmax = np.max(array)
+vmin = 2e-3
+
+def callback(fig, axes):
+    fig.text(0.657, 0.09, 'Iteration 3', fontsize=15, c='white')
+
+print('plotting ...')
+plot_arrays(
+    array, 
+    marker=plot_markers,
+    norm='log', 
+    vmin=vmin, 
+    vmax=vmax,
+    # callback=callback,
+    dpi=300,
+    **plot_dict,
+)
+
+#%%
+mdl = sky_mf
+smp = samples_mf
+
+sky_val = smp.mean(mdl) * CONV_FACTOR
+grd = mdl.grid
+sky_val = map_signal(grd, grd.update(space=grd.spc//2))(sky_val)
+
+mmin = 2*[2e-3] + 2*[4e-3]
+mmax = [5.5, 5.5, 11, 11]
+print(mmax)
+
+def callback(fig, axes):
+    fig.text(0.045, 0.950, '1012 MHz', fontsize=15, c='white')
+    fig.text(0.51, 0.950, '1112 MHz', fontsize=15, c='white')
+    fig.text(0.045, 0.485, '1368 MHz', fontsize=15, c='white')
+    fig.text(0.51, 0.485, '1427 MHz', fontsize=15, c='white')
+
+print('plotting ...')
+plot_arrays(
+    sky_val, 
+    rows=2,
+    norm='log',
+    vmin=mmin, 
+    vmax=mmax, 
+    grid_kwargs=dict(hspace=-0.71, wspace=-0.7, width_ratios=[1,1], height_ratios=[1,1]),
+    callback=callback,
+    dpi=200, 
+    **plot_dict,
+)
+
+#%%
+mdl = sky_mf
+smp = samples_mf
+
+sky_ref = smp.mean(mdl.ref_freq_model) * CONV_FACTOR
+mmin = 2e-3
+mmax = sky_ref.max()
+
+sky_pot = smp.mean(mdl.points_and_objects.ref_freq_model) * CONV_FACTOR
+alpha = smp.mean(mdl.points_and_objects.spectral_index)
+alpha = np.where(sky_pot > mmin, alpha-2, np.nan)
+amin = -3
+amax = 1
+
+contours = {'array': sky_ref, 'colors': 'white', 'levels': [1e-2, 1e-1], 'linewidths': 0.25}
+
+def callback(fig, axes):
+    fig.text(0.085, 0.90, '1112 MHz', fontsize=15, c='white')
+
+print('plotting ...')
+plot_arrays(
+    sky_ref, 
+    norm='log',
+    vmin=mmin,
+    vmax=mmax,
+    dpi=300,
+    callback=callback,
+    **plot_dict,
+)
+plot_arrays(
+    sky_pot, 
+    norm='log',
+    vmin=mmin, 
+    vmax=mmax,
+    dpi=300,
+    **plot_dict,
+)
+plot_arrays(
+    alpha, 
+    norm='linear',
+    vmin=amin,
+    vmax=amax,
+    dpi=300, 
+    contour=contours,
+    **plot_dict,
+)
+
+#%%
+mdl = sky_mf.components[1]
+smp = samples_mf
+
+sky_ref = smp.mean(mdl.ref_freq_model) * CONV_FACTOR
+
+alpha = smp.mean(mdl.spectral_index)
+alpha = np.where(sky_ref > mmin, alpha-2, np.nan)
+
+contours = {'array': sky_ref, 'colors': 'white', 'levels': [1e-2, 1e-1], 'linewidths': 0.5}
+
+print('plotting ...')
+plot_arrays(
+    sky_ref, 
+    norm='log',
+    vmin=mmin,
+    vmax=mmax,
+    dpi=300,
+    **plot_dict,
+)
+plot_arrays(
+    alpha, 
+    norm='linear',
+    vmin=amin,
+    vmax=amax,
+    dpi=300, 
+    contour=contours,
+    **plot_dict,
+)
+
+#%%
+mdl = sky_mf.components[2]
+smp = samples_mf
+
+sky_ref = smp.mean(mdl.ref_freq_model) * CONV_FACTOR
+
+alpha = smp.mean(mdl.spectral_index)
+alpha = np.where(sky_ref > mmin, alpha-2, np.nan)
+
+contours = {'array': sky_ref, 'colors': 'white', 'levels': [1e-2, 1e-1], 'linewidths': 0.5}
+
+print('plotting ...')
+plot_arrays(
+    sky_ref, 
+    norm='log',
+    vmin=mmin,
+    vmax=mmax,
+    dpi=300,
+    **plot_dict,
+)
+plot_arrays(
+    alpha, 
+    norm='linear',
+    vmin=amin,
+    vmax=amax,
+    dpi=300, 
+    contour=contours,
+    **plot_dict,
+)
+
+#%%
---
+++ steering/paper/eso_mf_zoom_talk.py
@@ -0,0 +1,376 @@
+#%%
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+
+#%%
+import jax
+import pickle
+import numpy as np
+from aim_resolve import OptimizeKLConfig, SetupKLConfig, SignalGrid, get_builders, plot_arrays, map_signal
+
+jax.config.update("jax_enable_x64", True)
+
+#%%
+def box_markers(cfg, ps_map, grid, it):
+    import numpy as np
+    from aim_resolve import draw_boxes
+
+    px, py = np.argwhere(ps_map > 0).T
+    ps_mrk = dict(x=px, y=py, s=10, c='white', marker='+')
+    box_map = draw_boxes(cfg.sections, grid, it)
+    ox, oy = np.argwhere(box_map > 0).T
+    oj_mrk = dict(x=ox, y=oy, s=0.1, c='white', marker=',')
+
+    return dict(ps_mrk=ps_mrk, oj_mrk=oj_mrk)
+
+#%%
+dir = '/scratch/users/rfuchs/packages/aim-resolve/steering/runs/fast_vi_1f_1024_1z_b'
+
+mf_rec = '3_rec_2z_6f'
+mf_it = 5
+
+ARCMIN2RAD = np.pi / 60 / 180
+AS2RAD = ARCMIN2RAD / 60
+CONV_FACTOR = 1000 * AS2RAD**2
+
+
+opt_yml = f'{dir}/opt/{mf_rec}/opt.yml'
+print('load:', opt_yml)
+
+optim_cfg = OptimizeKLConfig.from_file(opt_yml, get_builders, 'major')
+
+sky_mf = optim_cfg.instantiate_sec(f'sky.{mf_it}')
+print('sky components:', [c.prefix for c in sky_mf.models])
+
+freq = [f'{round(f/1e6)} MHz' for f in sky_mf.freq]
+print('sky freqs:', freq)
+ref_freq = freq[1]
+print('ref freq:', ref_freq)
+
+with open(f'{dir}/opt/{mf_rec}/last.pkl', "rb") as f:
+    samples_mf, *_ = pickle.load(f)
+print('samples:', len(samples_mf))
+
+setup_cfg = SetupKLConfig.from_file(opt_yml)
+sky_ps = sky_mf.points[0]
+ps_map = map_signal(sky_ps.points.grid, sky_ps.grid)(np.ones(sky_ps.shape)).sum(axis=0)
+markers_mf = box_markers(setup_cfg, ps_map, sky_mf.grid, mf_it)
+print('markers:', [f'{k}: {len(v["x"])}' for k, v in markers_mf.items()])
+
+#%%
+min_mf = 2.5e-3
+max_mf = np.max(samples_mf.mean(sky_mf.ref_freq_model) * CONV_FACTOR)
+print('vmin:', min_mf, '\nvmax:', max_mf)
+
+plot_dict = dict(
+    name = None,
+    odir = None,
+    norm = 'log',
+    vmin = min_mf,
+    vmax = max_mf,
+    cmap = 'inferno',
+    cbar = False,
+    ticks = 0,
+    dpi=300,
+)
+
+#%%
+sky_ref_mf = samples_mf.mean(sky_mf.ref_freq_model) * CONV_FACTOR
+
+pot_ref_mf = samples_mf.mean(sky_mf.points_and_objects.ref_freq_model) * CONV_FACTOR
+
+bg_ref_mf = samples_mf.mean(sky_mf.background.ref_freq_model) * CONV_FACTOR
+
+pot_alpha = samples_mf.mean(sky_mf.points_and_objects.spectral_index)
+pot_alpha = np.where(pot_ref_mf > 5e-3, pot_alpha, np.nan)
+amin, amax = -3, None
+
+contours = {'array': sky_ref_mf, 'colors': 'white', 'levels': [1e-2, 1e-1], 'linewidths': 0.25}
+
+print('plotting ...')
+plot_arrays(
+    array = sky_ref_mf, 
+    marker = markers_mf,
+    callback = lambda fig, ax: fig.text(0.085, 0.90, ref_freq, fontsize=15, c='white'),
+    **plot_dict,
+)
+plot_arrays(
+    array = sky_ref_mf, 
+    callback = lambda fig, ax: fig.text(0.085, 0.90, ref_freq, fontsize=15, c='white'),
+    **plot_dict,
+)
+plot_arrays(
+    array = bg_ref_mf, 
+    callback = lambda fig, ax: fig.text(0.085, 0.90, ref_freq, fontsize=15, c='black'),
+    **plot_dict,
+)
+plot_arrays(
+    array = pot_ref_mf, 
+    callback = lambda fig, ax: fig.text(0.085, 0.90, ref_freq, fontsize=15, c='black'),
+    **plot_dict,
+)
+plot_arrays(
+    array = pot_alpha, 
+    # callback = lambda fig, ax: fig.text(0.085, 0.90, 'Spectral Index', fontsize=15, c='black'),
+    contour = contours,
+    **plot_dict | dict(vmin=amin, vmax=amax, norm='linear'),
+)
+
+#%%
+sky_val_mf = samples_mf.mean(sky_mf) * CONV_FACTOR
+grd = sky_mf.grid
+sky_val = map_signal(grd, grd.update(space=grd.spc//2))(sky_val_mf)
+
+# mmin = 2*[2e-3] + 2*[4e-3]
+# mmax = [5.5, 5.5, 11, 11]
+# print(mmax)
+
+# def callback(fig, axes):
+#     fig.text(0.045, 0.950, '1012 MHz', fontsize=15, c='white')
+#     fig.text(0.51, 0.950, '1112 MHz', fontsize=15, c='white')
+#     fig.text(0.045, 0.485, '1368 MHz', fontsize=15, c='white')
+#     fig.text(0.51, 0.485, '1427 MHz', fontsize=15, c='white')
+
+print('plotting ...')
+plot_arrays(
+    sky_val, 
+    rows=2,
+    grid_kwargs=dict(hspace=-0, wspace=-0, width_ratios=[1,1,1], height_ratios=[1,1]),
+    # callback=callback,
+    **plot_dict,
+)
+
+#%%
+sky_val_mf = samples_mf.mean(sky_mf) * CONV_FACTOR
+grd = sky_mf.grid
+sky_val = map_signal(grd, grd.update(space=grd.spc//2))(sky_val_mf)
+
+print('plotting ...')
+for i,f in enumerate(freq):
+    plot_arrays(
+        sky_val[i], 
+        callback = lambda fig, ax: fig.text(0.085, 0.90, f, fontsize=15, c='white'),
+        **plot_dict,
+    )
+
+#%%
+comps_mf = [sky_mf.objects[0], sky_mf.objects[1]]
+
+comp_ref_mf = [samples_mf.mean(c.ref_freq_model) * CONV_FACTOR for c in comps_mf]
+comp_alpha = [samples_mf.mean(c.spectral_index) for c in comps_mf]
+min_cs, min_ca = 2.5e-3, 5e-3
+
+
+print('plotting ...')
+for c,a in zip(comp_ref_mf, comp_alpha):
+    a = np.where(c > min_ca, a, np.nan)
+    contours = {'array': c, 'colors': 'white', 'levels': [1e-2, 1e-1], 'linewidths': 0.5}
+    plot_arrays(
+        array = c, 
+        **plot_dict | dict(vmin=min_cs),
+    )
+    plot_arrays(
+        array = a,
+        contour=contours,
+        **plot_dict | dict(vmin=-3, vmax=0, norm='linear'),
+    )
+
+#%%
+import matplotlib.pyplot as plt
+
+min_scatter = 5e-3
+print('plotting scatter ...')
+for idx, (c_ref, c_alpha) in enumerate(zip(comp_ref_mf, comp_alpha), start=1):
+    mask = (c_ref > min_scatter) & np.isfinite(c_alpha)
+    x = c_ref[mask].ravel()
+    y = c_alpha[mask].ravel()
+
+    plt.figure()
+    plt.scatter(x, y, s=2, alpha=0.35, edgecolors='none')
+    plt.xlabel('Sky brightness [mJy/arcsec$^2$]')
+    plt.ylabel('Spectral index')
+    plt.title(f'Component {idx}')
+    plt.grid(alpha=0.2)
+
+#%%
+print('plotting scatter log scale ...')
+for idx, (c_ref, c_alpha) in enumerate(zip(comp_ref_mf, comp_alpha), start=1):
+    mask = (c_ref > min_scatter) & np.isfinite(c_alpha)
+    x = c_ref[mask].ravel()
+    y = c_alpha[mask].ravel()
+
+    plt.figure()
+    plt.scatter(x, y, s=2, alpha=0.35, edgecolors='none')
+    plt.xscale('log')
+    plt.xlabel('Sky brightness [mJy/arcsec$^2$]')
+    plt.ylabel('Spectral index')
+    plt.title(f'Component {idx}')
+    plt.grid(alpha=0.2)
+
+#%%
+# Identify different regions in component 1 based on spatial structure
+# Using 3x3 grid regionalization
+
+print('plotting scatter with 3x3 grid regions (log scale) ...')
+
+colors_9 = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple', 'magenta', 'brown']
+region_names = ['top-left', 'top-center', 'top-right', 
+                'mid-left', 'mid-center', 'mid-right',
+                'bot-left', 'bot-center', 'bot-right']
+
+for idx, (c_ref, c_alpha) in enumerate(zip(comp_ref_mf, comp_alpha), start=1):
+    mask = (c_ref > min_scatter) & np.isfinite(c_alpha)
+    
+    # Get pixel coordinates where mask is True
+    y_coords, x_coords = np.where(mask)
+    
+    # Divide into 3x3 grid
+    height, width = c_ref.shape
+    y_split = np.linspace(0, height, 4)
+    x_split = np.linspace(0, width, 4)
+    
+    # Assign region ID to each pixel
+    region_id = np.zeros(c_ref.shape, dtype=int)
+    for i in range(3):
+        for j in range(3):
+            grid_mask = ((y_coords >= y_split[i]) & (y_coords < y_split[i+1]) &
+                        (x_coords >= x_split[j]) & (x_coords < x_split[j+1]))
+            region_id[y_coords[grid_mask], x_coords[grid_mask]] = i * 3 + j
+    
+    x = c_ref[mask].ravel()
+    y = c_alpha[mask].ravel()
+    regions = region_id[mask].ravel()
+    
+    plt.figure(figsize=(10, 6))
+    for region in range(9):
+        region_mask = regions == region
+        if np.any(region_mask):
+            plt.scatter(x[region_mask], y[region_mask], s=2, alpha=0.5,
+                       edgecolors='none', color=colors_9[region], 
+                       label=region_names[region])
+    
+    plt.xscale('log')
+    plt.xlabel('Sky brightness [mJy/arcsec$^2$]')
+    plt.ylabel('Spectral index')
+    plt.title(f'Component {idx} (3x3 grid regions, log scale)')
+    plt.legend(loc='best', fontsize=7, ncol=3)
+    plt.grid(alpha=0.2)
+
+#%%
+# Create 2D image colored by 3x3 grid regions (same coloring as scatter plot)
+
+print('plotting 2D grid region maps ...')
+
+colors_9_rgb = np.array([
+    [1, 0, 0],        # red
+    [1, 0.647, 0],    # orange
+    [1, 1, 0],        # yellow
+    [0, 0.502, 0],    # green
+    [0, 1, 1],        # cyan
+    [0, 0, 1],        # blue
+    [0.627, 0, 1],    # purple
+    [1, 0, 1],        # magenta
+    [0.647, 0.165, 0] # brown
+])
+
+region_names = ['top-left', 'top-center', 'top-right', 
+                'mid-left', 'mid-center', 'mid-right',
+                'bot-left', 'bot-center', 'bot-right']
+
+for idx, (c_ref, c_alpha) in enumerate(zip(comp_ref_mf, comp_alpha), start=1):
+    mask = (c_ref > min_scatter) & np.isfinite(c_alpha)
+    
+    # Get pixel coordinates where mask is True
+    y_coords, x_coords = np.where(mask)
+    
+    # Divide into 3x3 grid
+    height, width = c_ref.shape
+    y_split = np.linspace(0, height, 4)
+    x_split = np.linspace(0, width, 4)
+    
+    # Assign region ID to each pixel
+    region_id = np.full(c_ref.shape, -1, dtype=int)  # -1 for masked out pixels
+    for i in range(3):
+        for j in range(3):
+            grid_mask = ((y_coords >= y_split[i]) & (y_coords < y_split[i+1]) &
+                        (x_coords >= x_split[j]) & (x_coords < x_split[j+1]))
+            region_id[y_coords[grid_mask], x_coords[grid_mask]] = i * 3 + j
+    
+    # Create RGB image colored by region
+    rgb_image = np.zeros((height, width, 3))
+    for region in range(9):
+        region_mask = region_id == region
+        rgb_image[region_mask] = colors_9_rgb[region]
+    
+    # Pixels outside the mask stay black
+    
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.imshow(rgb_image.transpose(1, 0, 2), origin='lower')
+    ax.set_title(f'Component {idx} (3x3 grid regions)')
+    
+    # Add legend for region colors
+    from matplotlib.patches import Patch
+    legend_elements = [Patch(facecolor=colors_9_rgb[i], label=region_names[i]) 
+                       for i in range(9)]
+    ax.legend(handles=legend_elements, loc='upper left', fontsize=7, ncol=3, 
+              bbox_to_anchor=(1.05, 1), frameon=True)
+    
+    plt.tight_layout()
+
+#%%
+# Bin by spectral index and sum brightness values
+
+print('plotting binned brightness vs spectral index ...')
+
+for idx, (c_ref, c_alpha) in enumerate(zip(comp_ref_mf, comp_alpha), start=1):
+    mask = (c_ref > min_scatter) & np.isfinite(c_alpha)
+    x = c_ref[mask].ravel()
+    y = c_alpha[mask].ravel()
+    
+    # Create bins for spectral index and sum brightness for each bin
+    n_bins = 100
+    binned_brightness, bin_edges = np.histogram(y, bins=n_bins, weights=x)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    
+    # Filter out empty bins
+    valid_mask = binned_brightness > 0
+    binned_brightness_valid = binned_brightness[valid_mask]
+    bin_centers_valid = bin_centers[valid_mask]
+    
+    plt.figure(figsize=(10, 6))
+    plt.scatter(binned_brightness_valid, bin_centers_valid, s=20, alpha=0.6, edgecolors='black', linewidth=0.5)
+    plt.xlabel('Summed sky brightness [mJy/arcsec$^2$]')
+    plt.ylabel('Spectral index')
+    plt.title(f'Component {idx} (binned by spectral index)')
+    plt.grid(alpha=0.2)
+
+#%%
+# Same as above but with log scale on brightness
+
+print('plotting binned brightness vs spectral index (log scale) ...')
+
+for idx, (c_ref, c_alpha) in enumerate(zip(comp_ref_mf, comp_alpha), start=1):
+    mask = (c_ref > min_scatter) & np.isfinite(c_alpha)
+    x = c_ref[mask].ravel()
+    y = c_alpha[mask].ravel()
+    
+    # Create bins for spectral index and sum brightness for each bin
+    n_bins = 100
+    binned_brightness, bin_edges = np.histogram(y, bins=n_bins, weights=x)
+    bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
+    
+    # Filter out empty bins
+    valid_mask = binned_brightness > 0
+    binned_brightness_valid = binned_brightness[valid_mask]
+    bin_centers_valid = bin_centers[valid_mask]
+    
+    plt.figure(figsize=(10, 6))
+    plt.scatter(binned_brightness_valid, bin_centers_valid, s=20, alpha=0.6, edgecolors='black', linewidth=0.5)
+    plt.xscale('log')
+    plt.xlabel('Summed sky brightness [mJy/arcsec$^2$]')
+    plt.ylabel('Spectral index')
+    plt.title(f'Component {idx} (binned by spectral index, log scale)')
+    plt.grid(alpha=0.2)
+
+# %%
